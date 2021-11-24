0a9e3976d0b45a905c58ac2a1b705cb42a3f33d1
Irbaaz <irbaazhussain49@gmail.com>
2021-11-24 14:34:41 +0530

    add paramscount varaiable in string

--- post.js
+++ post.js
@@ -1,1 +1,38 @@
 console.log('hello');
+
+let addedParamCount = 0;
+
+let paramertersBox = document.getElementById('parametersBox');
+paramertersBox.style.display = 'none';
+document.getElementById('requestJsonBox').style.display = 'none';
+document.getElementById('submit').style.display = 'none';
+
+let jsonRadio = document.getElementById('jsonRadio');
+jsonRadio.addEventListener('click', () => {
+    document.getElementById('parametersBox').style.display = 'none';
+    document.getElementById('requestJsonBox').style.display = 'block';
+    document.getElementById('submit').style.display = 'block';
+})
+
+let paramsRadio = document.getElementById('paramsRadio');
+paramsRadio.addEventListener('click', () => {
+    document.getElementById('requestJsonBox').style.display = 'none';
+    document.getElementById('parametersBox').style.display = 'block';
+    document.getElementById('submit').style.display = 'block';
+});
+
+let addParam = document.getElementById('addParam');
+addParam.addEventListener('click', () => {
+    let params = document.getElementById('params');
+    let string = `<div class="form-row">
+<label for="url" class="col-sm-2 col-form-label">Parameter 1</label>
+<div class="col-md-4">
+    <input type="text" class="form-control" id="parameterKey1" placeholder="Enter Parameter 1 Key">
+</div>
+<div class="col-md-4">
+    <input type="text" class="form-control" id="parameterValue1" placeholder="Enter Parameter 1 Value">
+</div>
+<button id="addParam" class="btn btn-primary">+</button>
+</div>`
+
+})
